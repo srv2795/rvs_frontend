c1fe20efe7ee0bad86d3bd052f0c47bfbf5e287f
Ran Vijay Singh <99132985+srv2795@users.noreply.github.com>
2024-03-18 23:48:33 +0530

    added date and author for news card

--- react_project/news-app/src/components/NewsItem.js
+++ react_project/news-app/src/components/NewsItem.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props
+    let {title, description, imageUrl, newsUrl, author, date} = this.props
     return (
       <div className="my-3">
         <div className="card">
             <img src={!imageUrl?"https://assets1.cbsnewsstatic.com/hub/i/r/2024/03/14/5f076666-0607-4206-bba5-cc58ef554e8f/thumbnail/1200x630/2634dad5774b04dcaec0c001962586c6/gettyimages-1412380301.jpg?v=d1d78866939020fc1f2607ef7298e4ec":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read more</a>
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <p className="card-text" ><small className="text-muted">By {!author ? "Unknown" : author} at {new Date(date).toGMTString()}</small></p>
+              <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
       </div>
