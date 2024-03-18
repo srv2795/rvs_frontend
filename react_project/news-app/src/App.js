31998f88466d98365efbfa5b86736e37c29e803d
Ran Vijay Singh <99132985+srv2795@users.noreply.github.com>
2024-03-18 07:48:03 +0530

    fixed landing page issue

--- react_project/news-app/src/App.js
+++ react_project/news-app/src/App.js
@@ -17,9 +17,9 @@ export default class App extends Component {
     return (
       <div>
         <BrowserRouter>
-        <Navbar />
+          <Navbar />
           <Routes>
-            <Route exact path="/home" element={<News key="home" pageSize={5} country="us" category="technology" />} />
+            <Route exact path="" element={<News key="home" pageSize={5} country="us" category="technology" />} />
             <Route exact path="/health" element={<News key="health" pageSize={5} country="us" category="health" />} />
             <Route exact path="/business" element={<News key="business" pageSize={5} country="us" category="business" />} />
             <Route exact path="/science" element={<News key="science" pageSize={5} country="us" category="science" />} />
