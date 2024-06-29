c01722560f485d26e7a04e4ba8a34a7d5bb21b92
Ran Vijay Singh <99132985+srv2795@users.noreply.github.com>
2024-06-29 13:49:03 +0530

    created an api in node to perform crud operation in mongo db

--- react_project/inotebook/backend/db.js
+++ react_project/inotebook/backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/";
 
 const connectToMongo = async () => {
- mongoose.connect(mongoURI, await console.log("Connected to mongo `Successful")
+ mongoose.connect(mongoURI, await console.log("Connected to mongo `Successfully")
     );
 }
 
--- react_project/inotebook/backend/index.js
+++ react_project/inotebook/backend/index.js
@@ -5,9 +5,14 @@ connectToMongo();
 const app = express()
 const port = 3000;
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/note', require('./routes/note'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
