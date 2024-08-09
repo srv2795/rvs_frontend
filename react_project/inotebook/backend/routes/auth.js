2d7fa26902d8fe10de9314353957a7464f2857cd
Ran Vijay Singh <99132985+srv2795@users.noreply.github.com>
2024-08-09 23:07:28 +0530

    added code to generate auth token with email and password

--- react_project/inotebook/backend/routes/auth.js
+++ react_project/inotebook/backend/routes/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-router.post('/',[
+router.post('/createuser',[
     body('name','Enter a valid name').isLength({min : 3}),
     body('email','Enter a valid email').isEmail(),
     body('password','password must be atleast 5 characters').isLength({min : 5}),
@@ -23,5 +23,43 @@ router.post('/',[
     .catch(err => console.log(err)
     ,res.json({error : 'Please enter a unique value of email'}));
 })
+
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','password can not be blank.').exists(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const{email, password} = req.body;
+    try{
+        let user = await user.findOne({email});
+        if(!user) {
+            return res.status(400).json({error : "please try to login with correct credential."});
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare) {
+            return res.status(400).json({error : "please try to login with correct credential."})
+        }
+
+        const data = {
+            user : {
+                is : user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({authToken})
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error..!")
+    }
+    
+})
+
 
 module.exports = router;
